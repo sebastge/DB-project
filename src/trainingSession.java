9506abb65754a2cd2a11d63ec1f7ea491c16c44e
Alfred <alfredlh@stud.ntnu.no>
2016-03-09 17:14:50 +0100

    Add small query

--- src/trainingSession.java
+++ src/trainingSession.java
@@ -8,14 +8,21 @@ public class trainingSession {
 
         try {
             // 1. Get connection to database
-            Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "");
+            Connection myCon = DriverManager.getConnection("jdbc:mysql://localhost/treningsdagbok", "root", "");
             // 2. Create a statement
             Statement myStmt = myCon.createStatement();
             // 3. Execute SQL query
+            //String sql = "SELECT navn FROM exercise " +
+            //        "JOIN trainingSession ON trainingSession.sessionID = exercise.trainingSession_sessionID" +
+            //        "JOIN "
             String sql = "insert into trainingSession  "
                     + " (sessionID, sessionDate, sessionTime, sessionDuration, personalForm, sessionIntent)"
-                    + " values ('1999', '10032016', '1230', '120', '7', 'Better stamina')";
+                    + " values ('1990', '10032016', '1230', '120', '7', 'Better stamina')";
             myStmt.executeUpdate(sql);
+            ResultSet res = myStmt.executeQuery("SELECT * FROM trainingSession");
+            while (res.next()){
+                System.out.println(res.getString("sessionDate") + ", " + res.getString("sessionTime"));
+            }
             System.out.println("Insert complete");
         } catch (Exception e) {
             e.printStackTrace();
